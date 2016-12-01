1832b0516f49d2c9786efeb2c04535cc17809b44
otoolebrian <brianotoole@gmail.com>
2016-12-01 21:05:18 +0100

    Changed to SoftwareSerial from Serial

--- esp-test/esp-test.ino
+++ esp-test/esp-test.ino
@@ -4,21 +4,22 @@
  * sitting on 192.168.0.13:12345 
   */
 
+
+#include<SoftwareSerial.h>
+SoftwareSerial softSerial(10,11); //RX,TX
+
 void setup() 
 {
   
   //Set the serial connection to be 9600
-  Serial.begin(9600);
+  softSerial.begin(9600);
 
   //Set up the ESP8266 Module, it should be visible on the AP
   //after these commands
 
   //Insert code to check, for the time being just delay 5 seconds
   //to allow the wifi to connect
-  delay(5000);
-
-  //Set the mode for the chip to be 
-  
+  delay(5000); 
   
 }
 
@@ -40,21 +41,21 @@ void loop()
 void sendInfo(String message, String IP, String port)
 {
   //Create the connection to the listening server
-  Serial.print("AT+CIPSTART=\"TCP\",\""+IP+"\","+port+"\r\n");
+  softSerial.print("AT+CIPSTART=\"TCP\",\""+IP+"\","+port+"\r\n");
 
   //Delay to allow it to set up
   delay(1000);
   
   //Send the message, and delay for one second to allow the
   //operation to complete
-  Serial.print("AT+CIPSEND="+String(message.length())+"\r\n");
+  softSerial.print("AT+CIPSEND="+String(message.length())+"\r\n");
   delay(1000);
  
   //Send the message
-  Serial.print(message+"\r\n");
+  softSerial.print(message+"\r\n");
   delay(1000);
  
   //Close the connection
-  Serial.print("AT+CIPCLOSE\r\n");
+  softSerial.print("AT+CIPCLOSE\r\n");
 }
 
